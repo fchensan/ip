2f11b939e4a70ffaaa08d1d6d924eec1a222d88a
Francisco Chensan <chensan@u.nus.edu>
2020-08-25 09:51:51 +0800

    Implement echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,9 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    private final static int EXIT = 0;
+    private final static int OK = 1;
 
     private static void printWelcomeMessage(){
         System.out.println("Hello! I'm Duke");
@@ -13,6 +18,18 @@ public class Duke {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
+    private static int handleInput(String input){
+        if(input.equals("bye")){
+            printByeMessage();
+            return EXIT;
+        }
+
+        System.out.println(input);
+        printDividerLine();
+
+        return OK;
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -23,6 +40,14 @@ public class Duke {
 
         printWelcomeMessage();
         printDividerLine();
-        printByeMessage();
+
+        Scanner in = new Scanner(System.in);
+        String input;
+
+        do {
+            System.out.print(">> ");
+            input = in.nextLine();
+        } while(handleInput(input) != EXIT);
+
     }
 }
